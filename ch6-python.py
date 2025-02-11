fca53d5803af4e3d7c00cb481755303fbc85b6a0
Aryanmahakur <mahakuraryan@gmail.com>
2025-02-11 16:01:17 +0530

    conditions

---
+++ ch6-python.py
@@ -0,0 +1,17 @@
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("Positive number")
+elif num < 0:
+    print("Negative number")
+else:
+    print("Zero")
+a = 1
+b=2
+c=3
+if a>b and a>c:
+    print("a is greater")
+elif b>a and b>c:
+    print("b is greater")
+else:
+    print("c is greater")
